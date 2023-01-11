00a4e50fcf620415150e257c09308b1eb30781ab
Benoit Jaunin <jaunin.b@fr.ibm.com>
2023-01-11 17:20:44 +0100

    fix typo

--- scripts/code-engine-utilities.sh
+++ scripts/code-engine-utilities.sh
@@ -343,7 +343,7 @@ setup-ce-env-entity_() {
     # shellcheck disable=SC2086
     if ibmcloud ce $kind get --name "$scope-$kind" > /dev/null 2>&1; then
       # configmap get does not fail if non existing - use the json output to ensure existing or no
-      if [ -z "$(ibmcloud ce $kind get --name "$scope-$kind" --output json | jq -r '.metatadata.name//empty')" ]; then
+      if [ -z "$(ibmcloud ce $kind get --name "$scope-$kind" --output json | jq -r '.metadata.name//empty')" ]; then
         echo "$kind $scope-$kind does not exist. Creating it"
         operation="create"
       else
